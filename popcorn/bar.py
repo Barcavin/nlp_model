ff72fb09dda5f525d489f552519bbf53717d051f
Kaiwen Dong <kevindong1994@gmail.com>
2018-03-01 18:21:50 -0600

    Create bar.py

---
+++ popcorn/bar.py
@@ -0,0 +1,11 @@
+import sys
+def drawProgressBar(percent, barLen = 20):
+    sys.stdout.write("\r")
+    progress = ""
+    for i in range(barLen):
+        if i < int(barLen * percent):
+            progress += "="
+        else:
+            progress += " "
+    sys.stdout.write("[ %s ] %.2f%%" % (progress, percent * 100))
+    sys.stdout.flush()
